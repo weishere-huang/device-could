c42b7263b8812363fb426a47c0b2158c46f77478
Niezx <420697454@qq.com>
2018-11-13 18:58:23 +0800

    服务IP

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'http://192.168.1.106:8080',
+        target: 'http://127.0.0.1:8080',
         pathRewrite: {
           "^/api": ""
         },
