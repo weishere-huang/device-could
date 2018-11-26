e14c2f2e03b85881d76be1c43897338163cc48b5
Niezx <420697454@qq.com>
2018-11-26 14:11:28 +0800

    ..

--- config/index.js
+++ config/index.js
@@ -15,7 +15,7 @@ module.exports = {
       '/api': {
         // target: 'http://www.hyazi.com:9881',
         //target: 'http://localhost:8888',
-         target: 'http://192.168.1.102:8080',
+         target: 'http://192.168.1.105:8080',
 
 
         pathRewrite: {
