120405f1b8e0626c56d4edb557bf30e07e2b6595
Niezx <420697454@qq.com>
2018-11-28 18:18:22 +0800

    修改

--- src/components/equipment/Equipment.vue
+++ src/components/equipment/Equipment.vue
@@ -90,7 +90,7 @@
     <div class="content">
       <div class="search">
         <el-button size="small" @click="toAdd">添加</el-button>
-        <el-button size="small" @click="redactShow">修改</el-button>
+        <el-button size="small" @click="editShow">修改</el-button>
         <el-button size="small" > 复制</el-button>
         <el-button size="small" @click="edelete">删除</el-button>
         <div class="searchright">
@@ -126,32 +126,7 @@ export default {
       pageSize: 9,
       ids: "",
       edbt:"",
-      tableData: [
-        {
-          name: "111",
-          tel: "222",
-          address: "3333",
-          hobby: "4444"
-        },
-        {
-          name: "111",
-          tel: "222",
-          address: "3333",
-          hobby: "4444"
-        }
-        // {
-        //   name:"111",
-        //   tel:"222",
-        //   address:"3333",
-        //   hobby:"4444"
-        // },
-        // {
-        //   name:"111",
-        //   tel:"222",
-        //   address:"3333",
-        //   hobby:"4444"
-        // }
-      ],
+      tableData: [],
       tableDate: [],
       columns: [
         {
