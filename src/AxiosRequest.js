a7da04d2caeaeb203f143a52e8697a1717c5adfb
黄伟 <huangwei10@jd.com>
2018-11-30 13:42:06 +0800

    修改通用请求类的bug

--- src/AxiosRequest.js
+++ src/AxiosRequest.js
@@ -1,15 +1,12 @@
 /*
  * @Author: weisheres.huang 
  * @Date: 2018-11-28 15:31:51 
- * @Last Modified by: mikey.zhaopeng
- * @Last Modified time: 2018-11-30 11:04:16
+ * @Last Modified by: weishere.huang
+ * @Last Modified time: 2018-11-29 11:10:11
  */
 import axios from 'axios';
 import global from './components/global/Global';
-import {
-  Message,
-  Loading
-} from 'element-ui';
+import { Message,Loading } from 'element-ui';
 axios.defaults.withCredentials = true;
 
 export default ({ url, type, params, config, option ,loadingConfig},vue) => {
@@ -25,35 +22,16 @@ export default ({ url, type, params, config, option ,loadingConfig},vue) => {
             resolve({ code: 0, data: params.resetData });
             return;
         }
-        loading.close();
-      }).catch(res => {
-        //error && error(res);
-        loading.close();
-        //console.log(info);
-        Message.error({
-          message: `网络异常：${res.message}`,
-          customClass: 'e-message',
-          duration: 5000
-        });
-        reject({
-          type: 'error',
-          info: res
-        });
-        // dispatch({
-        //     type: 'FAILED',
-        //     payload: res
-        // });
-        window.setTimeout(() => {
-          //2秒后自动重置错误信息
-          // dispatch({
-          //     type: 'RESET_FAILED'
-          // });
-        }, 2000);
-      })
-    }, 10)
+        window.setTimeout(()=>{
+            const loading=Loading.service(Object.assign({
+                lock: true,
+                text: '玩命加载中，请稍候...',
+                spinner: 'el-icon-loading',
+                background: 'rgba(255,255,255, 0.8)',
+                target: document.querySelector('.mainContentWrapper'),
+                customClass:'loadingStyle'
+                },loadingConfig||{}));
 
-
-<<<<<<< HEAD
                 axios[type||'post'](url, type==='get'?{params:params}:params, config || {}).then(res => {
                     if (res.status === 200 && res.data.code === 200) {
                         //success && success(res.data);
@@ -87,7 +65,4 @@ export default ({ url, type, params, config, option ,loadingConfig},vue) => {
         
         
     })
-=======
-  })
->>>>>>> c3bb3e8c634748570b6a32a6771a8eaccc6b9466
 }
