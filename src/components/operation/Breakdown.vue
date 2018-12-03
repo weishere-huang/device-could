7030090b611720fba66e202fd26f5549637de2e8
1305028734 <1305028734@qq.com>
2018-12-03 15:21:41 +0800

    运维管理-->设备故障

--- src/components/operation/Breakdown.vue
+++ src/components/operation/Breakdown.vue
@@ -2,9 +2,9 @@
   <div class="turnaround-plans">
     <div class="userCase">
       <div class="top">
-        <el-button size="small" @click="outerVisible = true">审核</el-button>
-        <el-button size="small" @click="revoke">故障消除</el-button>
-        <el-button size="small">删除</el-button>
+        <el-button size="small" @click="outerVisibleIsOk">审核</el-button>
+        <el-button size="small" @click="dispel">故障消除</el-button>
+        <el-button size="small" @click="deleteBreak">删除</el-button>
         <div class="search">
           <el-input type="search" placeholder="如故障编码，设备名称，位号，描述" size="small" v-model="faultKey"></el-input>
           <el-button size="small" @click="search">搜索</el-button>
@@ -23,26 +23,26 @@
     <el-dialog title="审核" :visible.sync="outerVisible">
       <el-form label-position=right label-width="120px" :model="formLabelAlign">
           <el-form-item label="审批结果：">
-            <el-radio v-model="approval" label="0">同意</el-radio>
-            <el-radio v-model="approval" label="1">驳回</el-radio>
+            <el-radio v-model="formLabelAlign.radio" label="0">同意</el-radio>
+            <el-radio v-model="formLabelAlign.radio" label="1">驳回</el-radio>
           </el-form-item>
           <el-form-item label="审批意见：">
             <el-input type="textarea" v-model="formLabelAlign.desc"></el-input>
           </el-form-item>
-          <div v-if="approval!=1">
+          <div v-if="formLabelAlign.radio!=1">
             <el-form-item label="是否终审：">
                 <el-checkbox-group v-model="formLabelAlign.type">
                     <el-checkbox label="" name="type"></el-checkbox>
                 </el-checkbox-group>
             </el-form-item>
             <el-form-item label="下一级审批人：" v-if="formLabelAlign.type!=true">
-                <el-input v-model="formLabelAlign.personnel" size="mini" style="width:60%"></el-input>
+                <el-input v-model="toAuditName.name" size="mini" style="width:60%"></el-input>
                 <el-button type="primary" @click="innerVisible = true" size="mini">添加审批人</el-button>
             </el-form-item>
           </div>
         </el-form>
     <el-dialog title="人员添加" :visible.sync="innerVisible" append-to-body>
-      <personnel></personnel>
+      <personnel v-on:getPersonnel="getPersonnel"></personnel>
     </el-dialog>
     <div slot="footer" class="dialog-footer">
       <el-button @click="outerVisible = false" size="mini">取 消</el-button>
@@ -58,8 +58,16 @@ import personnel from "./breakdown/Personnel";
 export default {
   data() {
     return {
-      formLabelAlign:{},
+      arr:[],
+      toAuditName:"",
+      formLabelAlign: {
+        desc:"",
+        type:"",
+        radio: "",
+        name:""
+      },
       approval:"",
+      faultIds:"",
       pageNumber:0,
       outerVisible: false,
       innerVisible: false,
@@ -158,11 +166,27 @@ export default {
       this.auditShow = true;
     },
     selectGroupChange(selection) {
-      console.log("select-group-change", selection);
+      this.arr = selection;
+      this.faultIds="";
+      for (let i=0;i<selection.length;i++){
+        if(this.faultIds ===""){
+          this.faultIds=selection[i].id;
+        }else{
+          this.faultIds+=","+selection[i].id;
+        }
+      }
       this.auditdetails=selection
     },
     selectALL(selection) {
-      console.log("select-aLL", selection);
+      this.arr = selection;
+      this.faultIds="";
+      for (let i=0;i<selection.length;i++){
+        if(this.faultIds ===""){
+          this.faultIds=selection[i].id;
+        }else{
+          this.faultIds+=","+selection[i].id;
+        }
+      }
     },
     selectChange(selection, rowData) {
       console.log("select-change", selection, rowData);
@@ -255,20 +279,6 @@ export default {
         }
       }
     },
-    revoke() {
-      let qs = require("qs");
-      let data = qs.stringify({
-        faultId: this.faultId
-      });
-      this.axios
-        .post(this.global.apiSrc + "/fault/revoke", data)
-        .then(response => {
-          console.log(response);
-        })
-        .catch(function(error) {
-          console.log(error);
-        });
-    },
     search() {
       this.axios
         .get(this.global.apiSrc + "/fault/search", {
@@ -283,7 +293,71 @@ export default {
         .catch(function(error) {
           console.log(error);
         });
-    }
+    },
+    dispel(){
+      let qs = require("qs");
+      let data = qs.stringify({
+        faultIds:this.faultIds,
+      });
+      this.axios
+        .post(this.global.apiSrc + "/fault/dispel",data)
+        .then(response =>{
+          alert(response.data.msg);
+          this.load();
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    },
+    deleteBreak(){
+      let qs = require("qs");
+      let data = qs.stringify({
+       ids:this.faultIds,
+      });
+      this.axios
+        .post(this.global.apiSrc + "/fault/delete",data)
+        .then(response =>{
+          alert(response.data.msg);
+          this.load();
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    },
+    getPersonnel(params) {
+      this.toAuditName = params.person;
+      this.innerVisible = params.hide;
+    },
+    submitAudit() {
+      this.formLabelAlign.type ? this.formLabelAlign.type = 0 : this.formLabelAlign.type = 1;
+      this.axios
+        .get(this.global.apiSrc + "/mplan/maintenanceAudit", {
+          params: {
+            passOrTurn: this.formLabelAlign.radio,
+            maintenanceId: this.maintenanceIds,
+            isEndAudit: this.formLabelAlign.type,
+            auditOpinion: this.formLabelAlign.desc,
+            nextUserId: this.toAudit.id
+          }
+        })
+        .then(response => {
+          this.arr="";
+          this.load();
+          this.outerVisible = false;
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+    outerVisibleIsOk() {
+      if (this.arr.length === 1) {
+        this.outerVisible = true
+      } else if(this.arr.length ===0){
+        alert("请选择计划")
+      }else{
+        alert("抱歉只能计划只能单个修改")
+      }
+    },
   },
   created() {
     this.load();
