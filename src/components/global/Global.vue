8f6262495e707f8c9b721a20cfa7a24e2b26fac6
Niezx <420697454@qq.com>
2018-12-03 18:11:01 +0800

    人员添加修改

--- src/components/global/Global.vue
+++ src/components/global/Global.vue
@@ -1,7 +1,8 @@
 <script>
 const apiSrc = "http://192.168.1.104:9880/d";
-// const apiSrc = "http://192.168.1.107:8080";
+// const apiSrc = "http://192.168.1.102:8080";
 export default {
+
   apiSrc
 };
 </script>
