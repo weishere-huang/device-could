5fdd176d113e4828b7f74fa18b5a1e4bbfd3ce3e
470538922 <470538922@qq.com>
2018-12-06 14:24:05 +0800

    备品备件

---
+++ src/components/sparePart/SparePartsWarehouse.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="spare-parts-warehouse">
+    <div class="top">
+      <el-button size="small">保存</el-button>
+    </div>
+    <div class="warehouse">
+      <h1>备件入库</h1>
+      <div>
+        <el-form
+          :inline="true"
+          :model="formInline"
+          class="demo-form-inline"
+          label-width="100px"
+        >
+          <el-form-item label="入库单号：" style="margin-bottom:0px;">
+            <el-input
+              v-model="formInline.user"
+              placeholder=""
+              size="small"
+              style="width:200px"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="入库日期：" style="margin-bottom:0px;">
+            <el-input
+              v-model="formInline.time"
+              placeholder=""
+              size="small"
+              style="width:200px"
+            ></el-input>
+          </el-form-item>
+        </el-form>
+      </div>
+      <div class="table-list">
+          <div>
+
+          </div>
+          <div>
+              
+          </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      formInline: {
+          time:new Date().toLocaleDateString(),
+      }
+    };
+  }
+};
+</script>
+
+<style lang="less" scoped>
+@blue: #409eff;
+@Success: #67c23a;
+@Warning: #e6a23c;
+@Danger: #f56c6c;
+@Info: #dde2eb;
+@border: 1px solid #dde2eb;
+.spare-parts-warehouse {
+  font-size: 12px;
+  .top {
+    border: @border;
+    border-radius: 5px;
+    padding: 10px;
+  }
+  .warehouse {
+    border: @border;
+    border-radius: 5px;
+    padding: 10px;
+    margin-top: 10px;
+    padding: 10px;
+    h1 {
+      text-align: center;
+      letter-spacing: 6px;
+    }
+  }
+}
+</style>
