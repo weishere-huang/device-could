1139674de6b13a95d61b311c3e4d4dc98a7491d1
470538922 <470538922@qq.com>
2018-12-07 08:58:38 +0800

    22

--- src/components/sparePart/SparePartsWarehouse.vue
+++ src/components/sparePart/SparePartsWarehouse.vue
@@ -70,31 +70,60 @@
               icon="el-icon-search"
             ></el-button>
           </el-input>
-          <div style="width:100%">
+          <div style="width:100%;margin-top:10px">
             <v-table
               :row-dblclick="toDetails"
               is-horizontal-resize
+              is-vertical-resize
               column-width-drag
               :multiple-sort="false"
-              style="width:100%;height:300px"
+              style="width:100%;height:280px"
+              :min-height='280'
               :columns="columns"
-              :table-data="tableData"
+              :table-data="tableData1"
               row-hover-color="#eee"
               row-click-color="#edf7ff"
             ></v-table>
           </div>
         </div>
-        <div style="float:left;font-size:20px">
+        <div style="float:left;font-size:20px;height:426px;line-height:426px">
           →
         </div>
         <div class="inventory-list">
-
+          <el-form label-width="85px">
+            <el-form-item
+              label="入库清单："
+              style="margin-bottom:0px;"
+            >
+            </el-form-item>
+          </el-form>
+          <div>
+            <v-table
+              :row-dblclick="toDetails"
+              is-horizontal-resize
+              is-vertical-resize
+              column-width-drag
+              :multiple-sort="false"
+              style="width:100%;height:364px"
+              :min-height='364'
+              :columns="columns1"
+              :table-data="tableData"
+              row-hover-color="#eee"
+              row-click-color="#edf7ff"
+              :cell-edit-done="cellEditDone"
+              @on-custom-comp="customCompFunc"
+            ></v-table>
+          </div>
+          <div style="color:#e6a23c;line-height:20px">
+            温馨提示：带"*"号的字段可编辑！录入完成后记得保存哟！
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
 <script>
+import Vue from "vue";
 export default {
   data() {
     return {
@@ -144,56 +173,129 @@ export default {
           overflowTitle: true
         }
       ],
-      tableData: [
+      columns1: [
         {
-          faultNo: "222"
+          field: "faultNo",
+          title: "备件名称",
+          width: 80,
+          titleAlign: "center",
+          columnAlign: "center",
+          isResize: true,
+          overflowTitle: true
         },
         {
-          faultNo: "222"
+          field: "faultNo",
+          title: "备件编号",
+          width: 80,
+          titleAlign: "center",
+          columnAlign: "center",
+          isResize: true,
+          overflowTitle: true
         },
         {
-          faultNo: "222"
+          field: "faultNo",
+          title: "备件型号",
+          width: 80,
+          titleAlign: "center",
+          columnAlign: "center",
+          isResize: true,
+          overflowTitle: true
         },
         {
-          faultNo: "222"
+          field: "faultNo",
+          title: "*数量",
+          width: 80,
+          titleAlign: "center",
+          columnAlign: "center",
+          isResize: true,
+          overflowTitle: true,
+          isEdit: true
         },
         {
-          faultNo: "222"
+          field: "faultNo",
+          title: "*单价（元）",
+          width: 80,
+          titleAlign: "center",
+          columnAlign: "center",
+          isResize: true,
+          overflowTitle: true,
+          isEdit: true
         },
         {
-          faultNo: "222"
+          field: "faultNo",
+          title: "金额",
+          width: 80,
+          titleAlign: "center",
+          columnAlign: "center",
+          isResize: true,
+          overflowTitle: true
         },
         {
-          faultNo: "222"
+          field: "faultNo",
+          title: "*供应商",
+          width: 80,
+          titleAlign: "center",
+          columnAlign: "center",
+          isResize: true,
+          overflowTitle: true,
+          isEdit: true
         },
         {
-          faultNo: "222"
+          field: "faultNo",
+          title: "*批次",
+          width: 80,
+          titleAlign: "center",
+          columnAlign: "center",
+          isResize: true,
+          overflowTitle: true,
+          isEdit: true
         },
         {
-          faultNo: "222"
+          field: "faultNo",
+          title: "*存放位置",
+          width: 80,
+          titleAlign: "center",
+          columnAlign: "center",
+          isResize: true,
+          overflowTitle: true,
+          isEdit: true
         },
         {
-          faultNo: "222"
+          field: "faultNo",
+          title: "*备注",
+          width: 80,
+          titleAlign: "center",
+          columnAlign: "center",
+          isResize: true,
+          overflowTitle: true,
+          isEdit: true
         },
         {
-          faultNo: "222"
-        },
-        {
-          faultNo: "222"
-        },
-        {
-          faultNo: "222"
-        },
-        {
-          faultNo: "222"
-        },
-        {
-          faultNo: "222"
-        },
-      ]
+          field: "custome-adv",
+          title: "操作",
+          width: 100,
+          titleAlign: "center",
+          columnAlign: "center",
+          componentName: "table-warehouse"
+          // isResize: true
+        }
+      ],
+      tableData: [],
+      tableData1: []
     };
   },
   methods: {
+    cellEditDone(newValue, oldValue, rowIndex, rowData, field) {
+      this.tableData[rowIndex][field] = newValue;
+      // 接下来处理你的业务逻辑，数据持久化等...
+    },
+    customCompFunc(params) {
+      if (params.type === "delete") {
+        console.log(params.rowData);
+        // this.deleteOne(params.rowData["id"]);
+        // this.$delete(this.tableData, params.index);
+      }
+    },
     toDetails(rowIndex, rowData, column) {
       console.log(rowData);
     },
@@ -205,6 +307,29 @@ export default {
     }
   }
 };
+Vue.component("table-warehouse", {
+  template: `<span>
+        <a href="" @click.stop.prevent="deleteRow(rowData,index)" style="text-decoration: none;">删除</a>
+        </span>`,
+  props: {
+    rowData: {
+      type: Object
+    },
+    field: {
+      type: String
+    },
+    index: {
+      type: Number
+    }
+  },
+  methods: {
+    deleteRow() {
+      // 参数根据业务场景随意构造
+      let params = { type: "delete", rowData: this.rowData };
+      this.$emit("on-custom-comp", params);
+    }
+  }
+});
 </script>
 
 <style lang="less" scoped>
@@ -236,13 +361,15 @@ export default {
       margin-top: 20px;
       .spare-parts-table {
         width: 30%;
-        border: @border;
+        // border: @border;
         float: left;
+        height: 426px;
       }
       .inventory-list {
         width: 68%;
-        border: @border;
+        // border: @border;
         float: left;
+        height: 426px;
       }
     }
   }
