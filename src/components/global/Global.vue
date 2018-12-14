3420d2bd4c8b692da739ff4f54b164a55e6e8360
470538922 <470538922@qq.com>
2018-12-14 09:59:45 +0800

    11

--- src/components/global/Global.vue
+++ src/components/global/Global.vue
@@ -1,6 +1,6 @@
 <script>
 // const apiSrc = "http://192.168.1.113:9880/d";
-const apiSrc = "http://192.168.1.104:9880/d";
+const apiSrc = "http://192.168.1.104:9880";
 const apiImg="http://192.168.1.148:8081";
 //const apiSrc = "http://192.168.1.148:8080";
 export default {
