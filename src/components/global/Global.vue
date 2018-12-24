77c68e579a0d84046ee1cbea3e88d0129be550dc
黄伟 <huangwei10@jd.com>
2018-12-24 16:14:11 +0800

    登录失效跳转，组织机构页面调整

--- src/components/global/Global.vue
+++ src/components/global/Global.vue
@@ -1,8 +1,8 @@
 <script>
-  const apiSrc = "http://192.168.1.104:9880";
+  const apiSrc = "http://127.0.0.1:8080/api";
   const imgPath = "http://192.168.1.104:8080/";
   const apiImg = "http://192.168.1.104:9886/upload";
-  // const apiSrc = "http://118.25.217.48:9880/api";
+  // const apiSrc = "http://118.25.217.48:9880";
   // const imgPath = "http://118.25.217.48:8080/";
   // const apiImg = "http://118.25.217.48:9886/upload";
   export default {
